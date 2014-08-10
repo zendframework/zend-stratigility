9b6ed9267a6cbad28d73b13fe6c68d85b88c6661
Matthew Weier O'Phinney <matthew@zend.com>
2014-08-10 11:25:14 -0500

    Tested Next functionality
    - All scenarios and behaviors

---
+++ test/NextTest.php
@@ -0,0 +1,117 @@
+<?php
+namespace PhlyTest\Conduit;
+
+use ArrayObject;
+use Phly\Conduit\Http\Request;
+use Phly\Conduit\Http\Response;
+use Phly\Conduit\Next;
+use Phly\Conduit\Route;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class NextTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->stack    = new ArrayObject();
+        $this->request  = new Request('1.1', 'php://memory');
+        $this->response = new Response();
+    }
+
+    public function testDoneHandlerIsInvokedWhenStackIsExhausted()
+    {
+        // e.g., 0 length array, or all handlers call next
+        $triggered = null;
+        $done = function ($err = null) use (&$triggered) {
+            $triggered = true;
+        };
+
+        $next = new Next($this->stack, $this->request, $this->response, $done);
+        $next();
+        $this->assertTrue($triggered);
+    }
+
+    public function testInvokesItselfWhenRouteDoesNotMatchCurrentUrl()
+    {
+        // e.g., handler matches "/foo", but path is "/bar"
+        $phpunit = $this;
+        $route = new Route('/foo', function ($req, $res, $next) use ($phpunit) {
+            $phpunit->fail('Route should not be invoked if path does not match');
+        });
+        $this->stack[] = $route;
+
+        $triggered = null;
+        $done = function ($err = null) use (&$triggered) {
+            $triggered = true;
+        };
+
+        $this->request->setUrl('http://local.example.com/bar');
+
+        $next = new Next($this->stack, $this->request, $this->response, $done);
+        $next();
+        $this->assertTrue($triggered);
+    }
+
+    public function testInvokesItselfIfRouteDoesNotMatchAtABoundary()
+    {
+        // e.g., if route is "/foo", but path is "/foobar", no match
+        $phpunit = $this;
+        $route = new Route('/foo', function ($req, $res, $next) use ($phpunit) {
+            $phpunit->fail('Route should not be invoked if path does not match');
+        });
+        $this->stack[] = $route;
+
+        $triggered = null;
+        $done = function ($err = null) use (&$triggered) {
+            $triggered = true;
+        };
+
+        $this->request->setUrl('http://local.example.com/foobar');
+
+        $next = new Next($this->stack, $this->request, $this->response, $done);
+        $next();
+        $this->assertTrue($triggered);
+    }
+
+    public function testInvokesHandlerWhenMatched()
+    {
+        // e.g., if route is "/foo", but path is "/foobar", no match
+        $triggered = null;
+        $route = new Route('/foo', function ($req, $res, $next) use (&$triggered) {
+            $triggered = true;
+        });
+        $this->stack[] = $route;
+
+        $phpunit = $this;
+        $done = function ($err = null) use ($phpunit) {
+            $phpunit->fail('Should not hit done handler');
+        };
+
+        $this->request->setUrl('http://local.example.com/foo');
+
+        $next = new Next($this->stack, $this->request, $this->response, $done);
+        $next();
+        $this->assertTrue($triggered);
+    }
+
+    public function testRequestUriInInvokedHandlerDoesNotContainMatchedPortionOfRoute()
+    {
+        // e.g., if route is "/foo", and "/foo/bar" is the original path,
+        // then the URI path in the handler is "/bar"
+        $triggered = null;
+        $route = new Route('/foo', function ($req, $res, $next) use (&$triggered) {
+            $triggered = $req->getUrl()->path;
+        });
+        $this->stack[] = $route;
+
+        $phpunit = $this;
+        $done = function ($err = null) use ($phpunit) {
+            $phpunit->fail('Should not hit done handler');
+        };
+
+        $this->request->setUrl('http://local.example.com/foo/bar');
+
+        $next = new Next($this->stack, $this->request, $this->response, $done);
+        $next();
+        $this->assertEquals('/bar', $triggered);
+    }
+}
