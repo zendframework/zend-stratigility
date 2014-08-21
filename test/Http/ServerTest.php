6a9f97a34c852282f8093f3933d2959fe4214eb7
Matthew Weier O'Phinney <matthew@zend.com>
2014-08-21 13:11:51 -0500

    Add more test coverage for Server

--- test/Http/ServerTest.php
+++ test/Http/ServerTest.php
@@ -39,6 +39,18 @@ class ServerTest extends TestCase
         $this->assertSame($this->response, $server->response);
     }
 
+    public function testCreateServerFromRequestWillCreateResponseIfNotProvided()
+    {
+        $server = Server::createServerFromRequest(
+            $this->middleware,
+            $this->request
+        );
+        $this->assertInstanceOf('Phly\Conduit\Http\Server', $server);
+        $this->assertSame($this->middleware, $server->middleware);
+        $this->assertSame($this->request, $server->request);
+        $this->assertInstanceOf('Phly\Conduit\Http\Response', $server->response);
+    }
+
     public function testCannotAccessArbitraryProperties()
     {
         $server = new Server(
@@ -92,6 +104,30 @@ class ServerTest extends TestCase
         $server->listen();
 
         $this->assertContains('HTTP/1.1 200 OK', Output::$headers);
+        $this->assertContains('Content-Type: text/plain', Output::$headers);
+        $this->assertEquals('FOOBAR', Output::$body);
+    }
+
+    public function testListenEmitsStatusHeaderWithoutReasonPhraseIfNoReasonPhrase()
+    {
+        $server = [
+            'HTTP_HOST' => 'example.com',
+            'HTTP_ACCEPT' => 'application/json',
+            'REQUEST_METHOD' => 'POST',
+            'REQUEST_URI' => '/foo/bar',
+            'QUERY_STRING' => 'bar=baz',
+        ];
+
+        $middleware = new Middleware();
+        $middleware->pipe(function ($req, $res) {
+            $res->setStatusCode(299);
+            $res->addHeader('Content-Type', 'text/plain');
+            $res->end('FOOBAR');
+        });
+        $server = Server::createServer($middleware, $server);
+        $server->listen();
+
+        $this->assertContains('HTTP/1.1 299', Output::$headers);
         $this->assertContains('Content-Type: text/plain', Output::$headers);
         $this->assertEquals('FOOBAR', Output::$body);
     }
