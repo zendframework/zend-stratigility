df3b0aded35ddc06d293e151970a8ba2d266f841
Matthew Weier O'Phinney <matthew@zend.com>
2014-08-21 11:44:44 -0500

    Improve Request test coverage
    - Covered all code paths

--- test/Http/RequestTest.php
+++ test/Http/RequestTest.php
@@ -2,6 +2,7 @@
 namespace PhlyTest\Conduit\Http;
 
 use Phly\Conduit\Http\Request;
+use Phly\Conduit\Http\Uri;
 use PHPUnit_Framework_TestCase as TestCase;
 
 class RequestTest extends TestCase
@@ -48,5 +49,52 @@ class RequestTest extends TestCase
         $uri = $this->request->getUrl();
         $this->assertInstanceOf('Phly\Conduit\Http\Uri', $uri);
         $this->assertEquals($url, $uri->uri);
+    }
+
+    public function testConstructorRaisesExceptionForInvalidStream()
+    {
+        $this->setExpectedException('InvalidArgumentException');
+        new Request('1.1', ['TOTALLY INVALID']);
+    }
+
+    public function testUnsetDoesNothingIfUserPropertyDoesNotExist()
+    {
+        unset($this->request->foobar);
+        $this->assertFalse(isset($this->request->foobar));
+    }
+
+    public function testCannotOverrideMethodOnceSet()
+    {
+        $this->request->setMethod('POST');
+        $this->setExpectedException('RuntimeException');
+        $this->request->setMethod('PATCH');
+    }
+
+    public function invalidUrls()
+    {
+        return [
+            'null'   => [null],
+            'true'   => [true],
+            'false'  => [false],
+            'int'    => [1],
+            'float'  => [1.1],
+            'array'  => [['foo']],
+            'object' => [(object) ['foo']],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidUrls
+     */
+    public function testCannotSetUrlWithInvalidType($url)
+    {
+        $this->setExpectedException('InvalidArgumentException', 'must be');
+        $this->request->setUrl($url);
+    }
+
+    public function testCannotSetUrlIfInvalid()
+    {
+        $this->setExpectedException('InvalidArgumentException', 'Invalid URL provided');
+        $this->request->setUrl('foo');
     }
 }
