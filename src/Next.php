977b4e4ccf8ebb54133002ba90bfa18d83aa3ac0
Matthew Weier O'Phinney <matthew@zend.com>
2017-01-23 11:57:44 -0600

    Updated license copyright for all changed files.

--- src/Next.php
+++ src/Next.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @link      https://github.com/zendframework/zend-stratigility for the canonical source repository
- * @copyright Copyright (c) 2015-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2015-2017 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   https://framework.zend.com/license New BSD License
  */
 
