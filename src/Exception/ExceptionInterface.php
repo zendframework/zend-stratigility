5a05b4c91203888309e543a98abf283fd9cd2150
webimpress <contact@webimpress.com>
2017-12-05 11:31:18 +0000

    Added ExceptionInterface as marker for package-specific exceptions

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-stratigility for the canonical source repository
+ * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-stratigility/blob/master/LICENSE.md New BSD License
+ */
+
+namespace Zend\Stratigility\Exception;
+
+/**
+ * Marker interface for package-specific exceptions.
+ */
+interface ExceptionInterface
+{
+}
