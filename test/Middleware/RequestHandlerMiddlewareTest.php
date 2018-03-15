305718d49e357f3a03e42d615d6388d0e0f6b1ca
webimpress <contact@webimpress.com>
2018-03-15 09:04:54 +0000

    Removed redundant imports

--- test/Middleware/RequestHandlerMiddlewareTest.php
+++ test/Middleware/RequestHandlerMiddlewareTest.php
@@ -13,7 +13,6 @@ use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Zend\Stratigility\Middleware\RequestHandlerMiddleware;
 
