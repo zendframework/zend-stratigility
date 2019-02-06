c4627f72ec0223329af37762d0e1c57d198f64ae
Matthew Weier O'Phinney <matthew@zend.com>
2019-02-06 11:39:02 -0600

    feat: remove "final" declaration from ErrorHandler
    Per #176, because the `ErrorHandler` adds a public method that is not in
    any interface, it cannot be easily mocked for testing purposes. This
    patch removes that declaration.

--- src/Middleware/ErrorHandler.php
+++ src/Middleware/ErrorHandler.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-stratigility for the canonical source repository
- * @copyright Copyright (c) 2016-2018 Zend Technologies USA Inc. (https://www.zend.com)
+ * @copyright Copyright (c) 2016-2019 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-stratigility/blob/master/LICENSE.md New BSD License
  */
 
@@ -68,7 +68,7 @@ use function set_error_handler;
  * Listeners are attached using the attachListener() method, and triggered
  * in the order attached.
  */
-final class ErrorHandler implements MiddlewareInterface
+class ErrorHandler implements MiddlewareInterface
 {
     /**
      * @var callable[]
